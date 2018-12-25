07cd40c76c4716c481ac2a4e80740308a6f69ff1
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2018-12-25 20:09:27 +0100

    fixed test for run_drift_checks

--- tests/testthat/test_run_drift_checks.R
+++ tests/testthat/test_run_drift_checks.R
@@ -3,7 +3,7 @@ context("Check run_drift_checks() function")
 test_that("Length of all checks",{
   library("DALEX2")
   library("ranger")
-  redict_function <- function(m,x,...) predict(m, x, ...)$predictions
+  predict_function <- function(m,x,...) predict(m, x, ...)$predictions
   model_old <- ranger(m2.price ~ ., data = apartments)
   model_new <- ranger(m2.price ~ ., data = apartments_test)
   res <- run_drift_checks(model_old, model_new,
